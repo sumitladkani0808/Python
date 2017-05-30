43f4bcab25496af35c858efed4608f320dda9222
deepakshinde18 <deepakshinde18@gmail.com>
2017-05-30 10:26:13 +0530

    Find value of xpath from given xml string

--- src/bin/temp/xml_xpath.py
+++ src/bin/temp/xml_xpath.py
@@ -1,6 +1,6 @@
 from lxml import etree
 
-xmlstring = """<?xml version="1.0"?>
+xml_string = """<?xml version="1.0"?>
 <?xml-stylesheet href="catalog.xsl" type="text/xsl"?>
 <!DOCTYPE catalog SYSTEM "catalog.dtd">
 <catalog>
@@ -46,25 +46,21 @@ xmlstring = """<?xml version="1.0"?>
     </product>
 </catalog>"""
 
-# xpathstring = '/catalog/product[@description="Cardigan Sweater"]/catalog_item[2]/size[3]/color_swatch[2]@image'
-# xpathstring = '/catalog/Header/Version'
-# xpathstring = '/catalog/product[@description="Cardigan Sweater"]/catalog_item[1]/item_number'
-# xpathstring = '/catalog/product[@description="Cardigan Sweater"]/catalog_item[2]/size[4]//color_swatch[1]/text()'
-xpathstring = '/catalog/product[@description="Cardigan Sweater"]/catalog_item[2]/size[4]//color_swatch[2]/text()'
-tree = etree.XML(xmlstring)
-etree.
-print(dir(tree))
-print(help(tree.makeelement))
-
+# xpath_string = '/catalog/product[@description="Cardigan Sweater"]/catalog_item[2]/size[3]/color_swatch[2]@image'
+xpath_string = '/catalog/Header/Version'
+# xpath_string = '/catalog/product[@description="Cardigan Sweater"]/catalog_item[1]/item_number'
+# xpath_string = '/catalog/product[@description="Cardigan Sweater"]/catalog_item[2]/size[4]//color_swatch[1]/text()'
+# xpath_string = '/catalog/product[@description="Cardigan Sweater"]/catalog_item[2]/size[4]//color_swatch[2]/text()'
+tree = etree.XML(xml_string)
 if tree is not None:
     try:
-        xpathvalueref = tree.xpath(xpathstring)
-        if not (hasattr(xpathvalueref[0], 'text')):
+        xpath_value_ref = tree.xpath(xpath_string)
+        if not (hasattr(xpath_value_ref[0], 'text')):
             print('IN IF CONDITION')
-            xpathvalue = xpathvalueref[0].strip()
+            xpath_value = xpath_value_ref[0].strip()
         else:
-            xpathvalue = xpathvalueref[0].text.strip()
             print('IN ELSE CONDITION')
-        print('xpathvalue is :- [{}]'.format(xpathvalue))
+            xpath_value = xpath_value_ref[0].text.strip()
+        print('xpath_value is :- [{}]'.format(xpath_value))
     except Exception as e:
         print('Error while getting Xpath :- [{}]'.format(e.error_log))
